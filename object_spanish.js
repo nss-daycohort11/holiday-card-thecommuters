f2af6c4ef8605311ed5914705094e4982f799684
micahp0506 <micahp0506@gmail.com>
2015-10-29 11:28:24 -0500

    Adding HTML changes to branch

---
+++ object_spanish.js
@@ -0,0 +1,39 @@
+$(document).ready(function() { 
+
+var Translate = (function(spanish) {
+  var spanish = {"merry":"feliz", "christmas":"navidad", "and":"y", "happy":"feliz", "new":"nuevo", "year":"ano"}
+  
+
+  
+  solarsystem.setSpanishTranslation = function () {
+    
+    
+    return closest_stars;
+  };
+
+  // solarsystem.getSolarsystemAge = function () {
+  //   return solarsystem_age;
+  // };
+
+  // solarsystem.setSolarsystemAge = function (age) {
+  //   if (age >= solarsystem_age) {
+  //     solarsystem_age = age;
+  //     this.last_modified = Date.now();
+  // } else {
+  //   throw "Age cannot be less than " + solarsystem_age;
+  // }
+  // };
+
+  // solarsystem.getDwarfPlanets = function () {
+  //   return dwarf_planets;
+  // }
+
+  // solarsystem.setDwarfPlanets = function (dwarf) {
+  //   dwarf_planets.push(dwarf);
+  //   this.last_modified = Date.now();
+  // }
+  // return solarsystem;
+
+})();
+
+});
