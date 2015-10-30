7c0ece32b3e341f2bd10796ac9dacc6907ce9cb6
jstiltner <jasonstiltner@gmail.com>
2015-10-29 23:15:16 -0500

    implimented ternary operations in formhander for coolness & tidied code

--- object_formhandler.js
+++ object_formhandler.js
@@ -1,38 +1,17 @@
 $(document).ready(function() {
-console.log(Translate);
 var userInputArray=[];
 var displayMessage = [];
-var whichLanguage 
+var lexi 
   $("#go").click(function(e){
-  	// Get input as a string
     var str = $("#userInput").val();
     userInputArray = str.split(" ");
-    whichLanguage = $("input:checked").attr('id');
-    // console.log(whichLanguage);
-    if (whichLanguage==="italian"){
-      Translate.translateToItalian(userInputArray);
-    }
-    else if (whichLanguage==='spanish'){
-      Translate.translateToSpanish(userInputArray);
-    }
-    else if (whichLanguage==='french')
-    {
-      Translate.translateToFrench(userInputArray);
-    }
-    else {window.alert("Please select a language, fool.")
-    }
-    
-  	
-  
-  	
+    lexi = $("input:checked").attr('id');
+    lexi==="italian"?
+      Translate.translateToItalian(userInputArray):
+    lexi==='spanish'?
+      Translate.translateToSpanish(userInputArray):
+    lexi==='french'?
+      Translate.translateToFrench(userInputArray):
+    window.alert("Please select a language, fool.");
   	});
-
-
-
-
-
-
-  
-
-  // All the JavaScript that depends on jQuery will be written here
 });
--- object_french.js
+++ object_french.js
@@ -1,36 +1,20 @@
 var Translate = (function(translate) {
     var french = {"merry":"Joyeux", "christmas":"noel", "and":"et", "happy":"bonne", "new":"nouvelle", "year":"an"};
-  
     translate.translateToFrench = function(userInputArray) {
-  
       var stringDisplay = "" ;
-    for (i=0; i<userInputArray.length; i++){
-  
-      var currentInput = userInputArray[i];
-      // displayMessage = french[currentInput];
-      stringDisplay += french[currentInput] + " ";
-      $("#container").html(stringDisplay);
-  
+      for (i=0; i<userInputArray.length; i++){
+        var currentInput = userInputArray[i];
+        stringDisplay += french[currentInput] + " ";
+        $("#container").html(stringDisplay);
       }
     return french[userInput];
     }
-
     translate.getVocab = function(){
-      return French;
+    return french
     }
-  return translate;
-
-  }
-
-
-  
-
-
- )
+return translate
+})
 (Translate);
 
-// console.log(solarSystem);
-// console.log(solarSystem.setAge(9));
-  // All the JavaScript that depends on jQuery will be written here
 
 
